5e1244de547b035708354ea5db0fb2b5f7bdb6be
Wrecx <85760230+Wrecx@users.noreply.github.com>
2025-08-10 22:58:02 +0200

    Add files via upload

---
+++ mods.js
@@ -0,0 +1,689 @@
+const mods = [
+  {
+    title: "The New Generation",
+    author: "LivajQ",
+    releaseDate: "2021-02-09",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/downloads/amnesia-the-new-generation"
+  }
+  ,
+  {
+    title: "Selphium",
+    author: "LivajQ ",
+    releaseDate: "2020-12-31",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/downloads/selphium12"
+  }
+  ,
+  {
+    title: "Tales of Survivalists",
+    author: "LivajQ ",
+    releaseDate: "2021-10-14",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/mods/tales-of-survivalists/downloads/tales-of-survivalists-release-10"
+  }
+  ,
+  {
+    title: "House Incident",
+    author: "LivajQ ",
+    releaseDate: "2019-11-24",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/downloads/house-incident-v12"
+  }
+  ,
+  {
+    title: "House Incident - Chapter 2",
+    author: "LivajQ",
+    releaseDate: "2020-04-05",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/downloads/house-incident-2"
+  }
+  ,
+  {
+    title: "Survival",
+    author: "LivajQ",
+    releaseDate: "2020-09-20",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/downloads/survival-12"
+  }
+  ,
+  {
+    title: "Ancient Showdown",
+    author: "LivajQ",
+    releaseDate: "2022-01-17",
+    type: "Custom Story",
+    downloadLink: "https://drive.google.com/file/d/1nYCIXYyK3uBSrC0KCX9_Rk98hYkqmQs6/view"
+  }
+  ,
+  {
+    title: "Coulrophobia",
+    author: "Skashi",
+    releaseDate: "2023-08-30",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/downloads/coulrophobia1"
+  }
+  ,
+  {
+    title: "Subtler teeth-grinding",
+    author: "DXN",
+    releaseDate: "2010-09-15",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/subtler-teeth-grinding-installation-file"
+  }
+  ,
+  {
+    title: "enclosedmap",
+    author: "Zlandael",
+    releaseDate: "2010-09-26",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/enclosedmap-my-first-map"
+  }
+  ,
+  {
+    title: "The Haunted House",
+    author: "Gamewatcher, Civilwo1f254",
+    releaseDate: "2010-10-18",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/the-haunted-house-beta"
+  }
+  ,
+  {
+    title: "Wake",
+    author: "DamnNoHtml",
+    releaseDate: "2010-11-05",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/wake"
+  }
+  ,
+  {
+    title: "Scroob's Castle Demo",
+    author: "GameWatcher",
+    releaseDate: "2010-11-15",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/scroob-s-castle-demo"
+  }
+  ,
+  {
+    title: "My Simple Mod",
+    author: "GameWatcher",
+    releaseDate: "2011-01-07",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/my-simple-mod-unnamed"
+  }
+  ,
+  {
+    title: "An Unexpected Arrival",
+    author: "Nye",
+    releaseDate: "2011-02-07",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/an-unexpected-arrival"
+  }
+  ,
+  {
+    title: "Zombie Escape",
+    author: "Havi95",
+    releaseDate: "2011-03-10",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/zombie-escape1"
+  }
+  ,
+  {
+    title: "Daniel's Sanity",
+    author: "Clord",
+    releaseDate: "2011-05-23",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/daniel-s-sanity"
+  }
+  ,
+  {
+    title: "Flail",
+    author: "Nkmol",
+    releaseDate: "2011-07-25",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/flail"
+  }
+  ,
+  {
+    title: "Bloody-axe",
+    author: "Nkmol",
+    releaseDate: "2011-07-25",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/bloody-axe"
+  }
+  ,
+  {
+    title: "Park bench",
+    author: "Nkmol",
+    releaseDate: "2011-07-25",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/park-bench"
+  }
+  ,
+  {
+    title: "Haycart(without hay)",
+    author: "Nkmol",
+    releaseDate: "2011-07-25",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/haycartwithout-hay"
+  }
+  ,
+  {
+    title: "Haycart",
+    author: "Nkmol",
+    releaseDate: "2011-07-25",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/haycart"
+  }
+  ,
+  {
+    title: "Hay",
+    author: "Nkmol",
+    releaseDate: "2011-07-25",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/hay"
+  }
+  ,
+  {
+    title: "Solid Sacrifice chapter 1",
+    author: "ArkhamGaming",
+    releaseDate: "2011-07-26",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/solid-sacrifice-chapter-1"
+  }
+  ,
+  {
+    title: "Custom Amnesia Menu Background",
+    author: "joshesully",
+    releaseDate: "2011-08-01",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/custom-amnesia-menu-background"
+  }
+  ,
+  {
+    title: "Recurring Nightmares ",
+    author: "Lumpa",
+    releaseDate: "2011-08-23",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/recurring-nightmares"
+  }
+  ,
+  {
+    title: "Wall Sconce",
+    author: "Selyp",
+    releaseDate: "2011-09-09",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/wall-sconce"
+  }
+  ,
+  {
+    title: "Industrial Fan",
+    author: "Selyp",
+    releaseDate: "2011-09-08",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/industrial-fan"
+  }
+  ,
+  {
+    title: "Amnesia Grunt Menu Background 1",
+    author: "willsage",
+    releaseDate: "2011-09-16",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/amnesia-grunt-menu-background-1"
+  }
+  ,
+  {
+    title: "ReportGuillotine",
+    author: "yourcomputer",
+    releaseDate: "2011-09-20",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/guillotine"
+  }
+  ,
+  {
+    title: "a Bath",
+    author: "Nkmol",
+    releaseDate: "2011-09-29",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/a-bath"
+  }
+  ,
+  {
+    title: "Chain-linked Fence Set",
+    author: "yourcomputer",
+    releaseDate: "2011-09-30",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/chain-linked-fence-set"
+  }
+  ,
+  {
+    title: "William's Castle",
+    author: "Rok Nardin",
+    releaseDate: "2012-03-31",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/william-s-castle"
+  }
+  ,
+  {
+    title: "The Experiment",
+    author: "Zokrar",
+    releaseDate: "2012-12-15",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/theexperiment"
+  }
+  ,
+  {
+    title: "Alexander Enemy",
+    author: "TheUnbeholden",
+    releaseDate: "2013-02-14",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/alexander-enemy"
+  }
+  ,
+  {
+    title: "DonProtz Misc",
+    author: "DonProtz",
+    releaseDate: "2013-02-15",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/donprotz-misc"
+  }
+  ,
+  {
+    title: "Modern Models",
+    author: "TheUnbeholden",
+    releaseDate: "2013-02-16",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/modern-models"
+  }
+  ,
+  {
+    title: "Adrenaline Syringe",
+    author: "ZodiacC",
+    releaseDate: "2013-02-22",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/adrenaline-syringe"
+  }
+  ,
+  {
+    title: "Creepy Girl",
+    author: "TheUnbeholden",
+    releaseDate: "2013-02-22",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/creepy-girl"
+  }
+  ,
+  {
+    title: "Acies's random things",
+    author: "TheUnbeholden",
+    releaseDate: "2013-02-22",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/acies-random-things"
+  }
+  ,
+  {
+    title: "Boat & Oars",
+    author: "Phoroneus",
+    releaseDate: "2013-09-05",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/boat-oars"
+  }
+  ,
+  {
+    title: "ReportCannon",
+    author: "Sel",
+    releaseDate: "2013-09-05",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/cannon"
+  }
+  ,
+  {
+    title: "DRedshot's entities",
+    author: "DRedshot",
+    releaseDate: "2013-09-05",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/dredshots-entities"
+  }
+  ,
+  {
+    title: "nkmol's various objects",
+    author: "Nkmol",
+    releaseDate: "2013-09-05",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/nkmols-various-objects"
+  } 
+  ,
+  {
+    title: "Pighead",
+    author: "Nkmol",
+    releaseDate: "2013-09-05",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/pighead"
+  }
+  ,
+  {
+    title: "Bullseye/Inspector lantern",
+    author: "Mofo",
+    releaseDate: "2013-09-05",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/bullseyeinspector-lantern"
+  }
+  ,
+  {
+    title: "The Chaser's Models",
+    author: "The Chaser",
+    releaseDate: "2013-09-16",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/the-chasers-models"
+  }
+  ,
+  {
+    title: "Trinity-A Game",
+    author: "ZuzaSparks",
+    releaseDate: "2013-09-19",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/httpwwwmoddbcommembersnathanial292addonst"
+  }
+  ,
+  {
+    title: "Flying Demon enemy",
+    author: "Measuring",
+    releaseDate: "2013-10-24",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/flying-demon-enemy"
+  }
+  ,
+  {
+    title: "Darkness chapter 1",
+    author: "Captainsimme",
+    releaseDate: "2014-07-04",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/amnesia-the-dark-decent-darkness-chapter-1"
+  }
+  ,
+  {
+    title: "Custom Main Menu Background",
+    author: "lalo5",
+    releaseDate: "2013-08-06",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/amnesia-custom-main-menu-background"
+  }
+  ,
+  {
+    title: "House of Brainless 1",
+    author: "nightbird74",
+    releaseDate: "2014-09-14",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/house-of-brainless-1"
+  }
+  ,
+  {
+    title: "House of Brainless 2",
+    author: "nightbird74",
+    releaseDate: "2014-09-14",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/house-of-brainless-2"
+  }
+  ,
+  {
+    title: "Dead Memories",
+    author: "Tman22099",
+    releaseDate: "2014-10-11",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/trevor-alves"
+  }
+  ,
+  {
+    title: "A Christmas Hunt",
+    author: "i3670",
+    releaseDate: "2014-12-29",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/a-christmas-hunt"
+  }
+  ,
+  {
+    title: "Hell's Slaughterhouse",
+    author: "Necroburger",
+    releaseDate: "2015-01-19",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/amnesia-hells-slaughterhouse-version-11"
+  }
+  ,
+  {
+    title: "Hell Castle",
+    author: "Necroburger",
+    releaseDate: "2015-02-05",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/amnesia-hell-castle"
+  }
+  ,
+  {
+    title: "Hell Castle 2",
+    author: "Necroburger",
+    releaseDate: "2015-02-13",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/amnesia-hell-castle-2"
+  }
+  ,
+  {
+    title: "Anne",
+    author: "Necroburger",
+    releaseDate: "2015-05-17",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/anne"
+  }
+  ,
+  {
+    title: "Colorful Candles and Lights (23 colors)",
+    author: "Kyle-The-Great, Umbakarna, Kiraimmortal, xAmn, TheScratchofLP",
+    releaseDate: "2015-07-27",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/colorful-candles-and-lights-24-colors"
+  }
+  ,
+  {
+    title: "Wanted",
+    author: "goalreadymc",
+    releaseDate: "2015-09-22",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/wanted-v1"
+  }
+  ,
+  {
+    title: "Subterranea",
+    author: "TheUnbeholden",
+    releaseDate: "2015-09-22",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/subterranea-demo"
+  }
+  ,
+  {
+    title: "Following The Wicked",
+    author: "DarkVolter",
+    releaseDate: "2016-09-12",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/following-the-wicked"
+  }
+  ,
+  {
+    title: "Smolnesia - The Dwarf Descent -",
+    author: "RoozyDB",
+    releaseDate: "2018-07-30",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/smolnesia-the-dwarf-descent"
+  }
+  ,
+  {
+    title: "Escape the prison",
+    author: "Biscotto39, Nonno Baldo",
+    releaseDate: "2018-09-16",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/escape-the-prison-21v"
+  }
+  ,
+  {
+    title: "New Insanity Ear Ring for AtDD",
+    author: "entropy_maker91",
+    releaseDate: "2019-01-09",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/new-insanity-ear-ring-for-atdd"
+  }
+  ,
+  {
+    title: "Player Death Sound",
+    author: "entropy_maker91",
+    releaseDate: "2019-01-09",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/player-death-sound"
+  }
+  ,
+  {
+    title: "Darkness (Main Menu)",
+    author: "FinnishRaccoon",
+    releaseDate: "2019-12-15",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/darkness-main-menu"
+  }
+  ,
+  {
+    title: "SOMA assets",
+    author: "pjgrich",
+    releaseDate: "2020-01-07",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/soma-assets"
+  }
+  ,
+  {
+    title: "MFP inventory GUI",
+    author: "Darkfires",
+    releaseDate: "2020-02-07",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/mfp-inventory-gui"
+  }
+  ,
+  {
+    title: "Colored Lanterns",
+    author: "RoozerXC",
+    releaseDate: "2020-06-30",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/colored-lanterns"
+  }
+  ,
+  {
+    title: "RanisCS Part 2",
+    author: "renis",
+    releaseDate: "2020-07-07",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/raniscs-part-2-20200707t085218z-001"
+  }
+  ,
+  {
+    title: "RanisCS Part 1",
+    author: "renis",
+    releaseDate: "2020-07-07",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/raniscs-part-1"
+  }
+  ,
+  {
+    title: "Amnesia TDD skybox fix for Linux",
+    author: "headlesscyborg",
+    releaseDate: "2021-04-08",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/amnesia-tdd-skybox-fix-for-linux"
+  }
+  ,
+  {
+    title: "HD Potatos",
+    author: "Saito720",
+    releaseDate: "2021-05-30",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/hd-potatoes2"
+  }
+  ,
+  {
+    title: "Bendy and the Ink Machine Assets",
+    author: "WalriderTriger",
+    releaseDate: "2021-07-13",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/bendy-and-the-ink-machine-assets"
+  }
+  ,
+  {
+    title: "Amnesia Hill Gui",
+    author: "AmkoXD, VengeanceFilms",
+    releaseDate: "2021-08-03",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/amnesia-gui"
+  }
+  ,
+  {
+    title: "Custom story lol",
+    author: "Roxeer",
+    releaseDate: "2021-08-05",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/custom-story-lol"
+  }
+  ,
+  {
+    title: "SCP-682",
+    author: "xarox02",
+    releaseDate: "2021-08-27",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/scp-6821"
+  }
+  ,
+  {
+    title: "RanisCS Part 3",
+    author: "renis",
+    releaseDate: "2021-11-10",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/raniscs-part-3"
+  }
+  ,
+  {
+    title: "Main Menu Playable",
+    author: "Maximogenio10",
+    releaseDate: "2022-03-09",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/main-menu-playable-11"
+  }
+  ,
+  {
+    title: "Funny one",
+    author: "paydayol77",
+    releaseDate: "2023-01-21",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/funny-one"
+  }
+  ,
+  {
+    title: "Amnesia of the Lost: King of Limbo!",
+    author: "AnchorCross",
+    releaseDate: "2023-02-10",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/amnesia-of-the-lost-king-of-limbo"
+  }
+  ,
+  {
+    title: "Hpl2 SOMA Construct Port",
+    author: "MrSheriff",
+    releaseDate: "2023-12-14",
+    type: "Addon",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/hpl2-soma-construct-port-v1"
+  }
+  ,
+  {
+    title: "House of Brainless 3",
+    author: "xarox02",
+    releaseDate: "2024-08-23",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/games/amnesia-the-dark-descent/addons/house-of-brainless-32"
+  }
+  ,
+  {
+    title: "shitass",
+    author: "",
+    releaseDate: "",
+    type: "Custom Story",
+    downloadLink: ""
+  }
+];
