f66770df21e1537ee0356229db129c62482cb150
Wrecx <85760230+Wrecx@users.noreply.github.com>
2025-08-12 02:47:53 +0200

    Update mods2.js

--- mods2.js
+++ mods2.js
@@ -10,8 +10,24 @@ const mods2 = [
   {
     title: "The Castle In The Woods",
     author: "M3NTALMAN",
-    releaseDate: "0000",
+    releaseDate: "????",
     type: "Custom Story",
     downloadLink: "https://www.moddb.com/mods/amnesia-project-old-forgotten-and-beginners-modes/downloads/amnesia-the-castle-in-the-woods"
+  }
+  ,
+  {
+    title: "Absurd",
+    author: "M3NTALMAN",
+    releaseDate: "????",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/mods/amnesia-project-old-forgotten-and-beginners-modes/downloads/absurd"
+  }
+  ,
+  {
+    title: "Absurd 2",
+    author: "M3NTALMAN",
+    releaseDate: "????",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/mods/amnesia-project-old-forgotten-and-beginners-modes/downloads/absurd-2-the-drugged-up-nightmare"
   }
 ];
