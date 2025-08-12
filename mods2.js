24e349a0011c55690ecc39ffd3ef883325e32630
Wrecx <85760230+Wrecx@users.noreply.github.com>
2025-08-12 16:49:42 +0200

    Update mods2.js

--- mods2.js
+++ mods2.js
@@ -118,4 +118,92 @@ const mods2 = [
     type: "Custom Story",
     downloadLink: "https://forum.frictionalgames.com/thread-8569.html"
   }
+  ,
+  {
+    title: "The Castle of Evil",
+    author: "czletsplay",
+    releaseDate: "????",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/mods/amnesia-project-old-forgotten-and-beginners-modes/downloads/the-castle-of-evil"
+  }
+  ,
+  {
+    title: "Diary of a Madman - Prologue",
+    author: "Zeravia Firestarter",
+    releaseDate: "2011",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/mods/amnesia-project-old-forgotten-and-beginners-modes/downloads/diary-of-madman-prologue"
+  }
+  ,
+  {
+    title: "The Omen of Destiny",
+    author: "D4veCZ",
+    releaseDate: "????",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/mods/amnesia-project-old-forgotten-and-beginners-modes/downloads/the-omen-of-destiny"
+  }
+  ,
+    {
+    title: "Where is Isaac?",
+    author: "",
+    releaseDate: "2014",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/mods/amnesia-project-old-forgotten-and-beginners-modes/downloads/escape-manson1"
+  }
+  ,
+  {
+    title: "Where am I?",
+    author: "czletsplay",
+    releaseDate: "2013",
+    type: "Custom Story",
+    downloadLink: "https://forum.frictionalgames.com/thread-22405-post-260598.html#pid260598"
+  }
+  ,
+  {
+    title: "Far Away",
+    author: "TheMightyJKK",
+    releaseDate: "????",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/mods/amnesia-project-old-forgotten-and-beginners-modes/downloads/far-away"
+  }
+  ,
+  {
+    title: "Rotten Manor",
+    author: "Sabatu",
+    releaseDate: "????",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/mods/amnesia-project-old-forgotten-and-beginners-modes/downloads/rotten-manor"
+  }
+  ,
+  {
+    title: "Map for Bizzeplayn",
+    author: "Wrecx",
+    releaseDate: "2020",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/mods/amnesia-project-old-forgotten-and-beginners-modes/downloads/map-for-bizzeplayn-demo"
+  }
+  ,
+  {
+    title: "The Black Eagle Castle",
+    author: "MidnightResearcher",
+    releaseDate: "2016",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/mods/amnesia-project-old-forgotten-and-beginners-modes/downloads/the-black-eagle-castle1"
+  }
+  ,
+  {
+    title: "Five Magics 2",
+    author: "Slanderous",
+    releaseDate: "????",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/mods/amnesia-project-old-forgotten-and-beginners-modes/downloads/fivemagics2"
+  }
+  ,
+  {
+    title: "The Old Well, Chapter 1",
+    author: "Vyzor",
+    releaseDate: "2011",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/mods/amnesia-custom-story-the-old-well/downloads/the-old-well-chapter-i"
+  }
 ];
