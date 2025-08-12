f97623ff2ecbd97cd2f6fe114bb8bda5c749573d
Wrecx <85760230+Wrecx@users.noreply.github.com>
2025-08-12 02:33:21 +0200

    Create mods2.js

---
+++ mods2.js
@@ -0,0 +1,17 @@
+const mods2 = [
+  {
+    title: "Madness Chapter 1",
+    author: "Marzio99",
+    releaseDate: "2013",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/mods/amnesia-project-old-forgotten-and-beginners-modes/downloads/madness-chapter-1"
+  }
+  ,
+  {
+    title: "The Castle In The Woods",
+    author: "M3NTALMAN",
+    releaseDate: "0000",
+    type: "Custom Story",
+    downloadLink: "https://www.moddb.com/mods/amnesia-project-old-forgotten-and-beginners-modes/downloads/amnesia-the-castle-in-the-woods"
+  }
+];
