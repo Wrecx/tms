2129d2d339211294a42837d847515b26f01f092a
Wrecx <85760230+Wrecx@users.noreply.github.com>
2025-08-13 17:44:29 +0200

    Update mods2.js

--- mods2.js
+++ mods2.js
@@ -1,98 +1,10 @@
 const mods2 = [
   {
-    title: "The Castle In The Woods",
-    author: "M3NTALMAN",
-    releaseDate: "????",
-    type: "Custom Story",
-    downloadLink: "https://www.moddb.com/mods/amnesia-project-old-forgotten-and-beginners-modes/downloads/amnesia-the-castle-in-the-woods"
-  }
-  ,
-  {
-    title: "In Memory",
-    author: "ʟogan",
-    releaseDate: "2011-04-22",
-    type: "Custom Story",
-    downloadLink: "https://www.moddb.com/mods/in-memory/"
-  }
-  ,
-  {
-    title: "Icarus - Prelude",
-    author: "ʟogan",
-    releaseDate: "2011-10-18",
-    type: "Custom Story",
-    downloadLink: "https://www.moddb.com/mods/icarus-prelude"
-  }
-  ,
-  {
-    title: "Vex's Nightmare",
-    author: "Vekkros",
-    releaseDate: "????",
-    type: "Custom Story",
-    downloadLink: "https://www.moddb.com/mods/amnesia-project-old-forgotten-and-beginners-modes/downloads/wexs-nightmare-demo"
-  }
-  ,
-  {
-    title: "Led Zuppalin",
-    author: "RoozyDB",
-    releaseDate: "????",
-    type: "Custom Story",
-    downloadLink: "https://www.moddb.com/mods/amnesia-project-old-forgotten-and-beginners-modes/downloads/led-zuppalin-test-map"
-  }
-  ,
-  {
-    title: "My Little Story (My Mod)",
-    author: "Nikus24",
-    releaseDate: "????",
-    type: "Custom Story",
-    downloadLink: "https://www.moddb.com/mods/amnesia-project-old-forgotten-and-beginners-modes/downloads/my-little-story-my-mod-test-map"
-  }
-  ,
-  {
-    title: "canis abubus",
-    author: "Skashi",
-    releaseDate: "????",
-    type: "Custom Story",
-    downloadLink: "https://forum.frictionalgames.com/thread-15193-post-135042.html"
-  }
-  ,
-  {
-    title: "Cry Oni Part 2",
-    author: "Vitor Frias (Vyzer)",
-    releaseDate: "2012",
-    type: "Custom Story",
-    downloadLink: "https://www.moddb.com/mods/amnesia-project-old-forgotten-and-beginners-modes/downloads/cry-oni-part-2"
-  }
-  ,
-  {
-    title: "Grunt's Castle",
-    author: "flamez3",
-    releaseDate: "2011",
-    type: "Custom Story",
-    downloadLink: "https://www.moddb.com/mods/amnesia-project-old-forgotten-and-beginners-modes/downloads/grunts-castle"
-  }
-  ,
-  {
     title: "It's Time to Troll",
     author: "willsage",
     releaseDate: "2012",
     type: "Custom Story",
     downloadLink: "https://www.youtube.com/watch?v=flQes8sATYw"
-  }
-  ,
-  {
-    title: "The Riddle of Vova",
-    author: "Laura_M",
-    releaseDate: "????",
-    type: "Custom Story",
-    downloadLink: "https://www.gamewatcher.com/mods/amnesia-the-dark-descent-mod/amnesia-the-riddle-of-vova"
-  }
-  ,
-  {
-    title: "Mini-Game: Hold Them Off!",
-    author: "laser50",
-    releaseDate: "2011",
-    type: "Custom Story",
-    downloadLink: "https://forum.frictionalgames.com/thread-8569.html"
   }
   ,
   {
@@ -119,22 +31,6 @@ const mods2 = [
     downloadLink: "https://www.moddb.com/mods/amnesia-project-old-forgotten-and-beginners-modes/downloads/the-omen-of-destiny"
   }
   ,
-    {
-    title: "Where is Isaac?",
-    author: "",
-    releaseDate: "2014",
-    type: "Custom Story",
-    downloadLink: "https://www.moddb.com/mods/amnesia-project-old-forgotten-and-beginners-modes/downloads/escape-manson1"
-  }
-  ,
-  {
-    title: "Where am I?",
-    author: "czletsplay",
-    releaseDate: "2013",
-    type: "Custom Story",
-    downloadLink: "https://forum.frictionalgames.com/thread-22405-post-260598.html#pid260598"
-  }
-  ,
   {
     title: "Far Away",
     author: "TheMightyJKK",
@@ -149,22 +45,6 @@ const mods2 = [
     releaseDate: "????",
     type: "Custom Story",
     downloadLink: "https://www.moddb.com/mods/amnesia-project-old-forgotten-and-beginners-modes/downloads/rotten-manor"
-  }
-  ,
-  {
-    title: "Map for Bizzeplayn",
-    author: "Wrecx",
-    releaseDate: "2020",
-    type: "Custom Story",
-    downloadLink: "https://www.moddb.com/mods/amnesia-project-old-forgotten-and-beginners-modes/downloads/map-for-bizzeplayn-demo"
-  }
-  ,
-  {
-    title: "The Black Eagle Castle",
-    author: "MidnightResearcher",
-    releaseDate: "2016",
-    type: "Custom Story",
-    downloadLink: "https://www.moddb.com/mods/amnesia-project-old-forgotten-and-beginners-modes/downloads/the-black-eagle-castle1"
   }
   ,
   {
